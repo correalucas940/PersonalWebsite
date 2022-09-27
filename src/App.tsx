7ffae3a2188d9599682ddb7f6575410747f9c4bd
Lucas Correa <correalucas940@gmail.com>
2022-09-27 14:42:39 -0400

    Added a clickable button and number increase

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,56 @@
-import {ImageBackground, StyleSheet, View, Text} from 'react-native';
-import image from "./image/family_chooser_tecnica_m.png"
+import React, {useState} from "react";
+import {ImageBackground, StyleSheet, View, Text, Button, Alert, TouchableOpacity, Image} from 'react-native';
+import image from "./image/family_chooser_tecnica_m.png";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Header } from "@rneui/themed";
 
 
 const styles = StyleSheet.create({
-  app: {
-    backgroundColor: 'black',
-    padding: 500,
+  container: {
+    backgroundColor: 'white',
   },
   text: {
     fontSize: 30,
     color: 'red',
   },
+  button: {
+    alignItems: "center",
+    backgroundColor: "gray",
+    padding: 10,
+  },
+  countContainer: {
+    alignItems: "center",
+    padding: 10,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 250,
+    resizeMode: 'contain'
+  },
 
 });
 
 const App = (): JSX.Element => {
+  const [count, setCount] = useState(0)
+  const onPress = () => setCount(prevCount => prevCount + 1);
+
   return (
-    <View style={styles.app}>
-    <ImageBackground
-      source={image}
-      style={{
-        width: '50%',
-        height: '50%',
-        position: 'absolute',
-      }} />
+    <View style={styles.container}>
+        <View style={styles.countContainer}>
+          <Text>Count: {count}</Text>
+        </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onPress}
+        >
+        <Text>Click me</Text>
+        </TouchableOpacity>
         <Text style={styles.text}> Building my first website </Text>
+        <Image
+        style={styles.image}
+        source={require('.//image/family_chooser_tecnica_m.png')}
+        />
       </View>
     
   );
